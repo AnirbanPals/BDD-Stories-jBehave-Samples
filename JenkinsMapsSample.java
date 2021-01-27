90d9545e02a34b76522533a680a14475b5658cbf
AP <11386539+AnirbanPals@users.noreply.github.com>
2021-01-27 23:12:21 +0530

    Create JenkinsMapsSample.java
    New Java File added to demostrate Maps and Lists

---
+++ JenkinsMapsSample.java
@@ -0,0 +1,42 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList; 
+import java.util.Iterator;
+
+public class JenkinsMapsSample {
+
+   Map m;
+   
+   public JenkinsMapsSample(){
+    m = new HashMap;
+   }
+   
+   public void initMap(){
+    m.put("one",1);
+    m.put("two",2);
+    m.put("three",3);
+    m.put("four",4);
+    m.put("five",5);
+   }
+   
+   public int countItems(){
+    return m.size();
+   }
+   
+   public List getItems(List keyList){
+     Iterator it = null;
+     List results = new ArrayList();
+     if (keyList != null)
+      Iterator it = keyList.getIterator();
+     else
+      return new ArrayList();
+    
+     String item = "";
+     while(it.hasNext()){
+      item = (String)it.next();
+      results.add(  Integer.parseInt( (m.get(item)!=null) ? item : "0") )  );
+     }
+     return results;
+   }
+}
